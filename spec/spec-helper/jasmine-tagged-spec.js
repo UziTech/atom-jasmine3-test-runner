647d8fe1c4d5ec3af8f799bbc64ecee6e51d1a32
Tony Brix <tony@brix.ninja>
2018-02-07 08:45:52 -0600

    add a few more specs

--- spec/spec-helper/jasmine-tagged-spec.js
+++ spec/spec-helper/jasmine-tagged-spec.js
@@ -1,16 +1,22 @@
 const env = jasmine.getEnv();
-if (env.setIncludedTags) {
-	env.setIncludedTags(["tagged", "tags"]);
-	env.includeSpecsWithoutTags(false);
-}
+const jasmineTagged = !!env.setIncludedTags && !!env.includeSpecsWithoutTags;
+
+const ran = jasmine.createSpy();
 
 describe("jasmine-tagged", function () {
+	if (jasmineTagged) {
+		env.setIncludedTags(["tagged", "tags"]);
+		env.includeSpecsWithoutTags(false);
+	}
+
 	it("is #tagged", function () {
 		pass();
+		ran();
 	});
 
 	it("has #multiple #tags", function () {
 		pass();
+		ran();
 	});
 
 	describe("pending", function () {
@@ -22,9 +28,34 @@ describe("jasmine-tagged", function () {
 			fail();
 		});
 	});
+
+	if (jasmineTagged) {
+		env.includeSpecsWithoutTags(true);
+	}
+
+	describe("including specs without tags", function () {
+		it("has no tags", function () {
+			expect(true).toBe(true);
+			ran();
+		});
+
+		it("still does #not run other tags", function () {
+			fail();
+		});
+	});
+
+	describe("describe #with tags", function () {
+		it("should not run this test", function () {
+			fail();
+		});
+	});
+
+	it("should run all passing tests", function () {
+		expect(ran).toHaveBeenCalledTimes(3);
+	});
 });
 
-if (env.setIncludedTags) {
+if (jasmineTagged) {
 	env.setIncludedTags();
 	env.includeSpecsWithoutTags(true);
 }
