c5186b88426337cdae17632074b1db51f08278e5
Tony Brix <tony@brix.ninja>
2018-02-08 16:47:53 -0600

    show deprecations

--- spec/spec-helper/attach-to-dom-spec.js
+++ spec/spec-helper/attach-to-dom-spec.js
@@ -1,21 +1,21 @@
 describe("attach-to-dom", function () {
 	beforeEach(function () {
-		this.jasmine2Content = document.getElementById("jasmine2-content");
+		this.jasmine3Content = document.getElementById("jasmine3-content");
 	});
 
 	it("should define jasmine.attachToDOM", function () {
 		expect(jasmine.attachToDOM).toEqual(jasmine.any(Function));
 	});
 
-	it("should attach jasmine2-content to the dom", function () {
+	it("should attach jasmine3-content to the dom", function () {
 
 		// uses jasmine-jquery
-		expect(this.jasmine2Content instanceof HTMLElement).toBe(true);
+		expect(this.jasmine3Content instanceof HTMLElement).toBe(true);
 	});
 
-	it("should append to jasmine2-content", function () {
+	it("should append to jasmine3-content", function () {
 		const element = document.createElement("div");
 		jasmine.attachToDOM(element);
-		expect(this.jasmine2Content.contains(element)).toBe(true);
+		expect(this.jasmine3Content.contains(element)).toBe(true);
 	});
 });
