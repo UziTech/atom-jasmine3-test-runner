766d4d4d721309b4da42d02bba1f175252564424
Tony Brix <tony@brix.ninja>
2019-04-14 23:22:54 -0500

    test(dev-deps): Remove dev-dependencies test

--- spec/reporter/dev-dependencies-spec.js
+++
@@ -1,9 +0,0 @@
-/** @babel */
-
-describe("dev-dependencies", function () {
-	it("should not have devDependencies by default", function () {
-		const pkg = require("../../package.json");
-
-		expect(pkg.devDependencies).toBeFalsy();
-	});
-});
