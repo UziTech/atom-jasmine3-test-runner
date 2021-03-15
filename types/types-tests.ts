e97d8792e3f9fa17a26564a60fafdb088624e474
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-03-15 12:01:46 -0500

    chore: add a type test for optionalConfigurationFunction

--- types/types-tests.ts
+++ types/types-tests.ts
@@ -1,9 +1,46 @@
 // The known examples that must work
 
 import { createRunner } from "./index";
+import type * as Jasmine from "jasmine";
+
+// Test 1
 
 createRunner({
 	testPackages: [],
 	timeReporter: true,
 	specHelper: true,
 });
+
+
+// Test 2
+
+// optional options to customize the runner
+const extraOptions = {
+	suffix: "-spec",
+	legacySuffix: "-spec-v1"
+};
+
+const optionalConfigurationFunction = function() {
+	// If provided, atom-jasmine3-test-runner will call this function before jasmine is started
+	// so you can do whatever you'd like with the global variables.
+	// (i.e. add custom matchers, require plugins, etc.)
+	require("some-jasmine-plugin");
+
+	beforeEach(function () {
+		jasmine.addMatchers({
+			toBeTheAnswerToTheUltimateQuestionOfLifeTheUniverseAndEverything: function (util, customEqualityTesters) {
+				return {
+					compare: function (actual) {
+						let result = {};
+						result.pass = util.equals(actual, 42, customEqualityTesters);
+						const toBeOrNotToBe = (result.pass ? "not to be" : "to be"); // that is the question.
+						result.message = `Expected ${actual} ${toBeOrNotToBe} the answer to the ultimate question of life, the universe, and everything.`;
+						return result;
+					}
+				};
+			}
+		});
+	});
+}
+
+createRunner(extraOptions, optionalConfigurationFunction);
