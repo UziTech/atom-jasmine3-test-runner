3f70ef8163c4cb5236d258e2e5155363b7aa656a
Tony Brix <tony@brix.ninja>
2021-03-24 11:05:11 -0500

    test: __esModule = true

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 
 import createRunner from "./lib/create-runner";
 
-exports.__esModule === true;
+exports.__esModule = true;
 exports.default = createRunner();
 exports.createRunner = createRunner;
