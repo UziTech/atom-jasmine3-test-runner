0d449ec35cbd2089b3fae65a393cbf799d5c271a
Tony Brix <tony@brix.ninja>
2021-04-26 00:26:13 -0500

    oops

--- spec/apm-spec.js
+++ spec/apm-spec.js
@@ -6,7 +6,7 @@ import { join } from "path";
 describe("apm", () => {
 	it("installs the dependencies in the correct test path", function () {
 		spawnSync("apm install atom-ide-base", { shell: true, stdio: "inherit", encoding: "utf8" });
-		let packages = readdirSync(join(atom.configDirPath, "packages));
+		let packages = readdirSync(join(atom.configDirPath, "packages"));
 		expect(packages).toContain("atom-ide-base");
 	});
 });
