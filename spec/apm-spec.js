ef5e6cd96197290602a1f5294af11420e317afc2
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-04-26 00:33:33 -0500

    fix: set env.ATOM_HOME to temp directory (#243)
    Co-authored-by: Tony Brix <tony@brix.ninja>

---
+++ spec/apm-spec.js
@@ -0,0 +1,12 @@
+"use babel";
+import { spawnSync } from "child_process";
+import { readdirSync } from "fs";
+import { join } from "path";
+
+describe("apm", () => {
+	it("installs the dependencies in the correct test path", function () {
+		spawnSync("apm install atom-ide-base", { shell: true, stdio: "inherit", encoding: "utf8" });
+		let packages = readdirSync(join(atom.configDirPath, "packages"));
+		expect(packages).toContain("atom-ide-base");
+	});
+});
